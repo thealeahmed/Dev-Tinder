949cdbd93ec1868532f718f5cdbee3b73ff1de57
thealeahmad <aliahmad111486@gmail.com>
2025-09-10 20:59:24 +0500

    exploring get,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
  const express=require("express")
 
  const app=express(); 
+  app.get("/user",(req,res)=>{
+   res.send({
+      firstName:"Ali",
+      lastName:"Ahmad"
+   })
+  })
+  app.post("/user",(req,res)=>{
 
- app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
- })
+   //DATA IS SAVED TO DB
+   res.send("Data successfully saved to the database")
+  })
 
- app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello ")
- })
- app.listen(3000,()=>{
-    console.log("server is successfully listening on port 3000...")
- });
-
+  app.delete("/user",(req,res)=>{
+   res.send("Data is deleted successfully")
+  })
+  app.patch("/user",(req,res)=>{
+   res.send("User is updated successfully")
+  })
+  app.put("/user",(req,res)=>{
+   res.send("Data is updated now Check..!!")
+  })
+app.listen(3000,()=>{
+   console.log('server is successfully listening on 3000...')
+})
   
