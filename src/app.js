7c56cd747b88369934db002b151fa5dea11d5795
thealeahmad <aliahmad111486@gmail.com>
2025-09-10 22:13:06 +0500

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
  const express=require("express")
 
  const app=express(); 
-  app.get("/user",(req,res)=>{
-   res.send({
-      firstName:"Ali",
-      lastName:"Ahmad"
-   })
-  })
-  app.post("/user",(req,res)=>{
+//   app.get("/user",(req,res)=>{
+//    res.send({
+//       firstName:"Ali",
+//       lastName:"Ahmad"
+//    })
+//   })
+//   app.post("/user",(req,res)=>{
 
-   //DATA IS SAVED TO DB
-   res.send("Data successfully saved to the database")
-  })
+//    //DATA IS SAVED TO DB
+//    res.send("Data successfully saved to the database")
+//   })
 
-  app.delete("/user",(req,res)=>{
-   res.send("Data is deleted successfully")
-  })
-  app.patch("/user",(req,res)=>{
-   res.send("User is updated successfully")
-  })
-  app.put("/user",(req,res)=>{
-   res.send("Data is updated now Check..!!")
-  })
+//   app.delete("/user",(req,res)=>{
+//    res.send("Data is deleted successfully")
+//   })
+//   app.patch("/user",(req,res)=>{
+//    res.send("User is updated successfully")
+//   })
+//   app.put("/user",(req,res)=>{
+//    res.send("Data is updated now Check..!!")
+//   })
+
+app.use("/user",[
+   (req,res,next)=>{
+   //Route Handler
+   res.send("Route Handler 1")
+   next();
+},(req,res)=>{ 
+   res.send("Route Handler 2")
+
+}]);
+
 app.listen(3000,()=>{
    console.log('server is successfully listening on 3000...')
 })
